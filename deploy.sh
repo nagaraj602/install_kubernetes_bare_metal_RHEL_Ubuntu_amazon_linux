439e5d7c17416094bf4022dffaf1a704048b3d58
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-08-01 00:14:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-set -e
+
+
+# Interactive Keypair Selection
+read -p "Do you want to use an existing AWS key pair? (y/n): " USE_EXISTING
+if [[ "$USE_EXISTING" =~ ^[Yy]$ ]]; then
+    read -p "Enter the name of the existing AWS Key Pair stored in AWS: " AWS_KEY_NAME
+    read -p "Enter the absolute path to your local private key (.pem) for this key pair: " PRIVATE_KEY_PATH
+    CREATE_NEW="false"
+else
+    echo "==> A new key pair will be generated automatically."
+    AWS_KEY_NAME=""
+    PRIVATE_KEY_PATH="../k8s-generated-key.pem"
+    CREATE_NEW="true"
+fi
 
 echo "==> Initializing and Applying Terraform..."
 cd terraform
 terraform init
-terraform apply -auto-approve
+terraform apply -auto-approve \
+  -var="create_new_key=$CREATE_NEW" \
+  -var="existing_key_name=$AWS_KEY_NAME" \
+  -var="private_key_path=$PRIVATE_KEY_PATH"
 
 echo "==> Waiting for EC2 instances to initialize SSH..."
 sleep 30
@@ -14,4 +30,9 @@ cd ../ansible
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i inventory.ini site.yml
 
-echo "==> Deployment Complete. Access your master node via the generated k8s-ssh-key.pem!"
+echo "==> Deployment Complete!"
+if [ "$CREATE_NEW" = "true" ]; then
+    echo "Access your master node using the newly generated key: k8s-generated-key.pem"
+else
+    echo "Access your master node using your existing key: $PRIVATE_KEY_PATH"
+fi

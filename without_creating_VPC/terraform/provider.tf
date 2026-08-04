62b81006bc8db4e1432997cb7f3f5a20777e887a
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-08-04 17:19:22 +0530

    Create provider.tf

---
+++ without_creating_VPC/terraform/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.15.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.5.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
